3ddc6fd3bbba3b90b003fc251024e763bfcbd8eb
iamaashi23 <101196053+iamaashi23@users.noreply.github.com>
2023-04-16 02:07:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/pages';
+import LoginPage from './components/pages/login';
+import ContactPage from './components/pages/contact';
+import PageNotFoundPage from './components/pages/pagenotfound';
+import ReqApi from './components/Login/API/ReqApi';
+import PostApi from './components/Login/API/PostApi'
+import Notepad from './components/Notepad'
+import Bmi from './components/pages/bmi';
+import Calorie from './components/pages/calorie'
+import Categories from './components/pages/categories';
+import Fit from './components/pages/excersisesformen';
+import Ref from './components/pages/toloosefat';
+import Fitness from './components/pages/workout';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Routes>
+        <Route path="/" element={<Home />} />  
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/contact" element={<ContactPage/>} />
+        <Route path="/*" element={<PageNotFoundPage/>} />
+         <Route path='/register' element={<ReqApi />}/>
+          <Route path= '/post'  element={<PostApi />}/>
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/excercisesformen" element={<Fit />} />
+          <Route path="/toloosefat" element={<Ref />} />
+          <Route path="/workout" element={<Fitness />} /> 
+          <Route path="/notepad" element={<Notepad />} />  
+          <Route path="/bmi" element={<Bmi/>} />
+          <Route path="/calorie" element={<Calorie/>} />
+        
+        </Routes>
+    </Router>
   );
 }
 
